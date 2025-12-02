9c961619673627420bedc3fff9414ca8df748882
hitzpro <wakhidaziz95192@gmail.com>
2025-12-02 11:28:24 +0700

    update qrcode & Config

--- src/config.js
+++ src/config.js
@@ -1,1 +1,5 @@
+// URL Backend
 export const API_BASE_URL = import.meta.env.PUBLIC_API_URL || "http://127.0.0.1:3200";
+
+// Fallback ke origin browser jika env tidak ada
+export const SITE_URL = import.meta.env.PUBLIC_SITE_URL || (typeof window !== 'undefined' ? window.location.origin : "http://localhost:4321");
